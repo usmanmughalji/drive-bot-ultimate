eafdc969572def8b2a45415b8509ea382d51f369
svr666 <sreerajvr24@gmail.com>
2020-06-30 10:46:16 +0530

    Modify the commands

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,23 +1,28 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
-        self.TarMirrorCommand = 'tarmirror'
+        self.MirrorCommand = 'mir'
+        self.UnzipMirrorCommand = 'unzip'
+        self.TarMirrorCommand = 'tarmir'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
         self.StatusCommand = 'status'
-        self.AuthorizeCommand = 'authorize'
-        self.UnAuthorizeCommand = 'unauthorize'
+        self.AuthorizeCommand = 'auth'
+        self.UnAuthorizeCommand = 'unauth'
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
         self.StatsCommand = 'stats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
         self.CloneCommand = "clone"
+<<<<<<< HEAD
         self.WatchCommand = 'watch'
         self.TarWatchCommand = 'tarwatch'
         self.deleteCommand = 'del'
+=======
+        self.WatchCommand = 'yt'
+        self.TarWatchCommand = 'taryt'
+>>>>>>> 3c84770... Modify the commands
 
 BotCommands = _BotCommands()
